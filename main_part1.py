5b0035098b3b70e5d8cf6cefa8053319eed32fc7
baglobaglo <baglosebastian@gmail.com>
2023-03-24 11:23:30 +0100

    LegendrePoly

--- main_part1.py
+++ main_part1.py
@@ -1,5 +1,34 @@
-from part1_task1 import combine_all_legendre_poly
+from part1_legendre_poly import create_legendre_poly_dict
+import numpy as np
+import math
+import pandas as panda
+import pygmt as ptgmt
 
+
+a = 6378137.0000 #m
+b = 6356752.3141 #m
+f_power_one = 298.257222101
+e_power_two = 0.006694380023
+Radius = 6371000.7900 #m
+GM_gravitational_parameter = 3986005*10**8 #m^3s^-2
+gravity_small_gamma = 9.81 #m s^-2
+
+#given values for J and J_hat
+J_N_2 = 0.108263*10**-2
+J_hat_N_2 = -(J_N_2)/np.sqrt(5)
+
+def J_hat_N_n(n):
+    #if(n == 0): return "Try with a different n"
+    if(n == 2): return J_hat_N_2
+    if(n % 2 == 0):
+        return (-1)**(n/2)*((3*np.exp(n)*(1-(n/2)+(5/2)*(J_N_2 / np.exp(2))*n))/((n+1)*(n+3)*np.sqrt(2*n+1)))
+    else: return 0
+#print(J_hat_N_n(4))
 
 def N_gravimetric(R, latitude, longitude, N_max, m):
     return #G*M/R*gamma
+
+#test1 = create_legendre_poly_dict(math.sin(math.radians(30.0)), 180)
+#print(test1[100, 89])
+
+#Now we collect data from the different file(s)
--- part1_legendre_poly.py
+++ part1_legendre_poly.py
@@ -1,34 +1,47 @@
 import numpy as np
 import math
+import pandas as panda
+import pygmt as ptgmt
+
+#conda activate pygmt
 
 
-#Geoid Height Computations from the Global Gravity Models
-#we start by defining some of the known and given constants
+#We use a dictionary to store all possible values for n and m
+#in way that may remind you of how it is done i matlab with matrices
 
-a = 6378137.0000 #m
-b = 6356752.3141 #m
-f_power_one = 298.257222101
-e_power_two = 0.006694380023
-Radius = 6371000.7900 #m
-GM_gravitational_parameter = 3986005*10**8 #m^3s^-2
-gravity_small_gamma = 9.81 #m s^-2
+def first_initial_legendre_poly_values(sin_phi_t):
+    legendre_poly_dict = {
+        (0, 0): 1,
+        (1, 0): sin_phi_t*math.sqrt(3),
+        (1, 1): math.sqrt(3)*math.sqrt(1-sin_phi_t**2),
+        (2, 0): math.sqrt(5)*((3/2)*sin_phi_t**2 - (1/2)),
+        (2, 1): sin_phi_t * math.sqrt(15)*math.sqrt(1-sin_phi_t**2)
+        }
+    return legendre_poly_dict
+#print(first_initial_legendre_poly_values(math.sin(math.radians(30.0))))
+
+#Now we define the functionality and logic for the different legendre polynomials
+
+def create_legendre_poly_dict(sin_phi_t, n):
+    initial_legendre_poly = first_initial_legendre_poly_values(sin_phi_t)
+    for i in range(2, n+1):
+        for j in range(i+1):
+            if (i>=3 and 1 <= j <= i-2):
+                initial_legendre_poly[(i, j)] = -math.sqrt(((2*i+1) * (i+j-1) * (i-j-1))/((2*i-3) * (i+j) * (i-j))) * initial_legendre_poly[(i-2, j)] + sin_phi_t * math.sqrt(((2*i+1) * (2*i-1))/((i+j)*(i-j))) * initial_legendre_poly[(i-1, j)]
+            if (i >= 2 and j == 0):
+                initial_legendre_poly[(i, j)] = -((math.sqrt(2*i+1))/(i)) * (i-1)/(math.sqrt(2*i-3)) * initial_legendre_poly[(i-2, 0)] + sin_phi_t * ((math.sqrt(2*i+1))/(i)) * math.sqrt(2*i-1) * initial_legendre_poly[(i-1, 0)]
+            if (i >= 2 and j == i):
+                initial_legendre_poly[(i, j)] = math.sqrt((2*i+1)/(2*i)) * math.sqrt(1-sin_phi_t**2) * initial_legendre_poly[(i-1, i-1)]
+            if (i >= 1 and j == i-1):
+                initial_legendre_poly[(i,j)] = sin_phi_t * math.sqrt(2*i + 1) * initial_legendre_poly[(i-1, i-1)]
+    return initial_legendre_poly
+
+#build_it = create_legendre_poly_dict(math.sin(math.radians(30.0)), 180)
+
+#print(build_it[100, 89])
 
 
-#next we try to define and structure the smaller function relying on eachother
-
-#given values for J and J_hat
-J_N_2 = 0.108263*10**-2
-J_hat_N_2 = -(J_N_2)/np.sqrt(5)
-
-def J_hat_N_n(n):
-    if(n == 0): return "Try with a different n"
-    if(n == 2): return J_hat_N_2
-    elif(n % 2 == 0):
-        return (-1)**(n/2)*((3*np.exp(n)*(1-(n/2)+(5/2)*(J_N_2 / np.exp(2))*n))/((n+1)*(n+3)*np.sqrt(2*n+1)))
-    else: return 0
-#print(J_hat_N_n(4))
-
-
+"""
 #Next we define the different cases of the Normalized Legendre's polynomials
 # t is defined as sin(latitude)
 def norm_legendre_poly_part1(n, t):
@@ -84,7 +97,7 @@ def norm_legendre_poly_part3(n, t):
         #p_n_m += -math.sqrt(((2*n+1)*(n+m-1)*(n-m-1))/((2*n-3)*(n+m)*(n-m)))*norm_legendre_poly_part4(n-2, t) + t*math.sqrt(((2*n+1)*(2*n-1))/((n+m)*(n-m)))*norm_legendre_poly_part3(n-1, t)
         #return p_n_m
 
-"""def norm_legendre_poly_part2_3(n, m, t):
+def norm_legendre_poly_part2_3(n, m, t):
     #we try to do this next part iterative as well.
     #this function handles cases where n >= 3 and 1<= m <= n-2
     if(n == 1 & m == 1):
@@ -92,7 +105,7 @@ def norm_legendre_poly_part3(n, t):
     if(n == 2 & m == 1):
         return t*math.sqrt(15)*math.sqrt(1-t**2)
     p_n_m = -(math.sqrt(((2*n+1)*(n+m-1)*(n-m-1))/((2*n-3)*(n+m)*(n-m))))*norm_legendre_poly_part2_3(n-2, m, t) + t*math.sqrt(((2*n+1)*(2*n-1))/((n+m)*(n-m)))*norm_legendre_poly_part2_3(n-1, m, t)
-    return p_n_m"""
+    return p_n_m
 
 #print(norm_legendre_poly_part2_3(3, 1, math.sin(math.radians(30.0))))
 
@@ -110,7 +123,7 @@ def norm_legendre_poly_part2(n, m, t):
                 + t*math.sqrt(((2*n+1)*(2*n-1))/((n+m)*(n-m)))*norm_legendre_poly_part2(n-1, m, t)
     else:
         raise ValueError(f"Invalid values of n={n} and m={m}")
-#print(norm_legendre_poly_part2(17, 5, math.sin(math.radians(30))))  # Computes the value for n=3, m=1 and t=0.5
+print(norm_legendre_poly_part2(17, 5, math.sin(math.radians(30))))  # Computes the value for n=3, m=1 and t=0.5
 
 def noram_legendre_poly_part2_test(n, m, t):
     if n == 0 and m == 0:
@@ -155,7 +168,7 @@ def norm_legendre_poly_new_test(n, m, t):
             n_1 = n_0
         return n_0
 
-print(norm_legendre_poly_new_test(50, 34, math.sin(math.radians(30))))  # Computes the value for n=3, m=1 and t=0.5
+#print(norm_legendre_poly_new_test(50, 34, math.sin(math.radians(30))))  # Computes the value for n=3, m=1 and t=0.5
 
 
 
@@ -172,3 +185,4 @@ def combine_all_legendre_poly(n, m, t):
 
 
 #print(combine_all_legendre_poly(100, 89, math.sin(math.radians(30))))
+"""
